494c6e98d15211f1f148675aecb216ca6dab3b51
= <56016006+thebush99@users.noreply.github.com>
2020-11-17 17:53:51 +0700

    update more

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
-export default layout;
+
+class Layout extends Component {
+  state = {
+    sideDrawer: true,
+  };
+  sideDrawerHandler = () => {
+    this.setState({ sideDrawer: false });
+  };
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawer: !prevState.sideDrawer };
+    });
+  };
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.sideDrawer}
+          closed={this.sideDrawerHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+export default Layout;
--- burger-builder/src/components/UI/Button/Button.js
+++ burger-builder/src/components/UI/Button/Button.js
@@ -3,8 +3,7 @@ import classes from './Button.module.css';
 const button = (props) => (
   <button
     onClick={props.clicked}
-    className={[classes.Button, classes[props.btnType]].join(' ')}
-    onClick={props.clicked}>
+    className={[classes.Button, classes[props.btnType]].join(' ')}>
     {props.children}
   </button>
 );
